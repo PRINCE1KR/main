4db3b2871e6de3f7b2b28c691014c94d8997ba72
PRINCE1KR <prince.kumar1@nexturn.com>
2023-01-28 16:45:49 +0530

    first commit

---
+++ src/Addition.java
@@ -0,0 +1,9 @@
+public class Addition {
+    public int add(int a,int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        Addition sum = new Addition();
+        System.out.println("addition of number-"+sum.add(2,3));
+    }
+}
---
+++ src/HelloPrince.java
@@ -0,0 +1,6 @@
+public class HelloPrince {
+    public static void main(String[] args) {
+        System.out.println("Welcome sir");
+        System.out.println("Welcome sir Prince");
+    }
+}
